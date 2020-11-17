f1e6ac5ed03a84e5616c4f772d152e0b24447afc
Brian Li <Brian.Li96@yahoo.com>
2020-11-17 17:40:37 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
+require 'pry'
 # code here!
+class School
+   attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else
+            @roster[grade] = [name]
+        end
+    end
+
+    def grade(age)
+        @roster[age]
+    end
+
+    def sort
+        sortedHash = {}
+        @roster.sort_by.each do |grade, name|
+            sortedHash[grade] = name.sort
+        end
+        sortedHash
+    end
+end
+
